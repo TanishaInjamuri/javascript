bd2c674583acd3dc512f7adfae5993c558839705
TanishaInjamuri <injamurit@gmail.com>
2025-08-20 18:12:15 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict";  // treat all  js code as newer version 
+
+//alert(3 + 3) // we are using nodejs, not browser 
+
+console.log(3 
+    + 
+    3)  // code readbility should be high 
+
+console.log("Tanisha") 
+
+let name = "Tanisha"
+let age = 18 
+let isLoggedIn = false
+
+// number => 2 to power 53    range 
+// bigint  => mostly use bigint in trading , stock market , big website , when you want big intger value  
+// string  => ""  '' you can use both 
+// boolean => true / false eg :- you got reponse from server or not , credit card is valid or not 
+
+/* null => standalone value  reprensation of empty value   
+eg ;- when server send repects and said tell me Ac temporty 
+so its send 0 so the server gets any problem  so that time we don't want 0 so we reprentent null */
+
+
+// undefined => means you are not defined the value eg :- let state ; i have delcare varaible but not value 
+
+// symbol => it is use for uniqunees find that time it is very usefull
+
+
+
+// object 
+
+console.log(typeof "Tanisha");  // showing the datatype -- so it is  string
+console.log(typeof 6);   // datatype so its number 
+
+console.log(typeof null);// so when you run this its show it is object 
+
+console.log(typeof undefined) // its show undefined 
